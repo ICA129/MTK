853996f839803397e6f0d693edbd651e185475e1
ICA129 <51882241+ICA129@users.noreply.github.com>
2019-06-19 20:49:43 +0700

    Create Mtk.py

---
+++ Mtk.py
@@ -0,0 +1,26 @@
+#!/bin/usr/python 
+import os
+import sys 
+from time import sleep
+
+os.system(' clear ')
+print("""\033[1;36m -----------------------------------------|
+|      TOOLS BELAJAR MATEMATIK                 |   
+|______________________________________________|                                              |
+|      JANGAN LUPA SUBCRIBE CHANNEL GUA        |
+|______________________________________________|                                              |
+|                                              |                 
+|      Author     = Mr.hmei24 && Mr.money      |
+|      Youtube   = ID [ICA ¤ HMEI24]           |     
+|      Info      = Gunakan Dengan Bijak Yee ^_^|
+-----------------------------------------------|""")
+sleep (5)
+print ("\033[1;36m MOHON TUNGGU SEBENTAR ")
+sleep(3)
+os.system(' pkg install python2 -y ')
+os.system(' pkg install git -y ')
+os.sytem(' git clone https://github.com/pashayogi/MTK-V2 ')
+os.system(' cd MTK-V2 ')
+print("\033[1;36m SHILAKAN MASUKAN PILIHAN ANDA")
+print("\033[1;36m CONTOH (python2 xxxxx.py)")
+os.system(' ls ')
