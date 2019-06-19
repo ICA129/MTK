22c7e04ffb91dfdd7bba06bbede88176647579ee
ICA129 <51882241+ICA129@users.noreply.github.com>
2019-06-19 21:14:26 +0700

    Update Mtk.py

--- Mtk.py
+++ Mtk.py
@@ -4,25 +4,11 @@ import sys
 from time import sleep
 
 os.system(' clear ')
-print("""\033[1;36m 
------------------------------------------------|
-|      TOOLS BELAJAR MATEMATIKA                |   
-|______________________________________________|                                              | 
-|   JANGAN LUPA SUBCRIBE CHANNEL GUA           |
-|______________________________________________|                                              |
-|                                              |                 
-|      Author     = Mr.hmei24 && Mr.money      |
-|      Youtube   = ID [ICA ¤ HMEI24]           |     
-|      Info      = Gunakan Dengan Bijak Yee ^_^|
------------------------------------------------|""")
-sleep (4)
+
 print ("\033[1;36m MOHON TUNGGU SEBENTAR ")
 print("\033[1;36m SEDANG MELAKUKAN INSTALASI")
 sleep(3)
 os.system(' pkg install python2 -y ')
 os.system(' pkg install git -y ')
-os.sytem(' git clone https://github.com/pashayogi/mtk-V1 ')
-os.system(' cd mtk-V1 ')
-print("\033[1;36m SHILAKAN MASUKAN PILIHAN ANDA")
-print("\033[1;36m CONTOH (python2 xxxxx.py)")
-os.system(' ls ')
+print("\033[1;36m TOOLS SEDANG DI PERBAHARUI ")
+os.system('reboot')
